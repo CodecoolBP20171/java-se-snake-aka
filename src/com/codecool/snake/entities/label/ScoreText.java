b66cd1aa5ff5840b731e09efdd1fcd88b5d536e4
ollepeter <mr.peterolle@gmail.com>
2017-09-12 11:36:27 +0200

    show counting score
    scoreText  value updated

--- src/com/codecool/snake/entities/label/ScoreText.java
+++ src/com/codecool/snake/entities/label/ScoreText.java
@@ -1,16 +1,28 @@
 package com.codecool.snake.entities.label;
 
 import com.codecool.snake.Globals;
+import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.entities.GameEntity;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
-public class ScoreText extends GameEntity {
+public class ScoreText extends GameEntity implements Animatable {
+    private Text scoreText;
 
     public ScoreText(Pane pane) {
         super(pane);
-        String score = "Score: " + String.valueOf(Globals.getScore());
-        Text scoreText = new Text(30, 30, score);
+        scoreText = new Text();
         pane.getChildren().add(scoreText);
     }
+
+    public void step() {
+        // For TEST only
+        Globals.setScore(Globals.getScore() + 1);
+        
+        String score = "Score: " + String.valueOf(Globals.getScore());
+        scoreText.setX(20);
+        scoreText.setY(30);
+        scoreText.setText(score);
+    }
+
 }
