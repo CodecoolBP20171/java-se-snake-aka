efa8ac007555541d6022ce421e1cbccd9b50c13b
ollepeter <mr.peterolle@gmail.com>
2017-09-12 10:48:59 +0200

    implemented show score feature

--- src/com/codecool/snake/entities/label/ScoreText.java
+++ src/com/codecool/snake/entities/label/ScoreText.java
@@ -3,19 +3,14 @@ package com.codecool.snake.entities.label;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.GameEntity;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 public class ScoreText extends GameEntity {
 
     public ScoreText(Pane pane) {
         super(pane);
-        setX(50);
-        setY(50);
-        pane.getChildren().add(this);
-        System.out.println(Globals.getScore());
+        String score = "Score: " + String.valueOf(Globals.getScore());
+        Text scoreText = new Text(30, 30, score);
+        pane.getChildren().add(scoreText);
     }
-
-
-
-
-
 }
