bc3280c9757088c336859630db5acb418d76901f
davidtanacs <tanacsdavid90@gmail.com>
2017-09-12 10:53:20 +0200

    Made scoretext more visible.

--- src/com/codecool/snake/entities/label/ScoreText.java
+++ src/com/codecool/snake/entities/label/ScoreText.java
@@ -3,6 +3,7 @@ package com.codecool.snake.entities.label;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.GameEntity;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class ScoreText extends GameEntity {
@@ -10,7 +11,8 @@ public class ScoreText extends GameEntity {
     public ScoreText(Pane pane) {
         super(pane);
         String score = "Score: " + String.valueOf(Globals.getScore());
-        Text scoreText = new Text(30, 30, score);
+        Text scoreText = new Text(5, 30, score);
+        scoreText.setFont(Font.font("Verdana", 30));
         pane.getChildren().add(scoreText);
     }
 }
